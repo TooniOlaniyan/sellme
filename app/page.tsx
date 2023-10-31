098d6270f1c5b0ac19907851d18029fe59f72c4c
TooniOlaniyan <salamlatif@gmail.com>
2023-10-31 21:53:16 +0100

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   <div>
-    Home
-   </div>
+   <section className='flex-start flex-col mb-16 paddings'>
+    <h1>Categories</h1>
+    <h1>Post</h1>
+    <h1>Loadmore</h1>
+
+   </section>
   )
 }
