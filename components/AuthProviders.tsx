538b694eaa9b727238e3a481153d2eb8bb7ed74c
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-02 09:47:53 +0100

    auth

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+'use client'
+import { useState , useEffect } from "react"
+import { signIn , getProviders } from "next-auth/react"
+
+type Provider = {
+  id: string,
+  name: string,
+  type: string,
+  signinUrl: string,
+  callbackUrl: string,
+  signinUrlParams?: Record<string , string> | null
+}
+type Providers = Record<string , Provider>
 
 const AuthProviders = () => {
+  const [providers , setProviders] = useState<Providers | null>(null)
   return (
     <div>AuthProviders</div>
   )
