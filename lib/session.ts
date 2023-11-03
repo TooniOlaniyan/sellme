8ae8ae07ec2ba11e21a49cf29ba31a153fa9ef27
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-03 18:13:28 +0100

    google auth completion

--- lib/session.ts
+++ lib/session.ts
@@ -8,19 +8,19 @@ import Jwt  from 'next-auth/jwt'
 export const authOptions : NextAuthOptions = {
     providers : [
         GoogleProvider({
-            clientId:'',
-            clientSecret:''
+            clientId:'process.env.GOOGLE_CLIENT_ID!',
+            clientSecret:'process.env.GOOGLE_CLIENT_SECRET!'
         })
     ],
 
-    jwt : {
-        encode: ({secret , token}) => {
+    // jwt : {
+    //     encode: ({secret , token}) => {
 
-        },
-        decode : async ({secret , token}) => {
+    //     },
+    //     decode : async ({secret , token}) => {
 
-        }
-    },
+    //     }
+    // },
 
     theme:{
         colorScheme: 'light',
@@ -29,9 +29,22 @@ export const authOptions : NextAuthOptions = {
 
     callbacks :{
         async session({session}) {
+            return session
             
         },
-        async signIn({user}) {
+        async signIn({user} : {user : AdapterUser | User} ) {
+            try {
+                //fetch user
+                // if user doesnt exixt we create a new user
+                // return user
+
+                return true
+                
+            } catch (error) {
+                // something went wrong
+                return false
+                
+            }
             
         }
     }
