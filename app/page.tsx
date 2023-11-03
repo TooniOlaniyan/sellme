0fa8d3b86eeb670fe392a529ea0645ff39a95cb5
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-03 15:31:54 +0100

    change

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
    <section className='flex-start flex-col mb-16 paddings'>
-    <h1>Categories</h1>
+    <h1>Categorie</h1>
     <h1>Post</h1>
     <h1>Loadmore</h1>
 
