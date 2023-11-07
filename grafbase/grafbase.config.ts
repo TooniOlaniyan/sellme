717afeba02d4bafbdcb8adf95aa6d68b8d0fe3c5
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-07 22:33:39 +0100

    setting the schema

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -12,20 +12,28 @@ import { g, auth, config } from '@grafbase/sdk'
 // Define Data Models
 // https://grafbase.com/docs/database
 
-const post = g.type('Post', {
-  title: g.string(),
-  
+const User = g.model("User", {
+  name: g.string().length({min: 2 , max:2}),
+  email: g.string().unique(),
+  avaterUrl: g.url(),
+  description: g.string(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  project: g.relation(() => Project).list().optional(),
+});
+
+const Project = g.model('Project', {
+  title: g.string().length({min:3}),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User)
 })
+
 
 
 export default config({
   schema: g
-  // Integrate Auth
-  // https://grafbase.com/docs/auth
-  // auth: {
-  //   providers: [authProvider],
-  //   rules: (rules) => {
-  //     rules.private()
-  //   }
-  // }
 })
