cffa627a5131d68fa9c9d3ec0063ba9834c2e740
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-08 13:16:31 +0100

    keys and secret

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,4 +1,4 @@
-import { g, auth, config } from '@grafbase/sdk'
+import { g, auth, config } from "@grafbase/sdk";
 
 // Welcome to Grafbase!
 // Define your data models, integrate auth, permission rules, custom resolvers, search, and more with Grafbase.
@@ -12,28 +12,50 @@ import { g, auth, config } from '@grafbase/sdk'
 // Define Data Models
 // https://grafbase.com/docs/database
 
-const User = g.model("User", {
-  name: g.string().length({min: 2 , max:2}),
-  email: g.string().unique(),
-  avaterUrl: g.url(),
-  description: g.string(),
-  githubUrl: g.url().optional(),
-  linkedInUrl: g.url().optional(),
-  project: g.relation(() => Project).list().optional(),
+//@ts-ignore
+
+const User = g
+  .model("User", {
+    name: g.string().length({ min: 2, max: 2 }),
+    email: g.string().unique(),
+    avaterUrl: g.url(),
+    description: g.string(),
+    githubUrl: g.url().optional(),
+    linkedInUrl: g.url().optional(),
+    project: g
+      .relation(() => Project)
+      .list()
+      .optional(),
+  })
+  .auth((rules) => {
+    rules.public().read();
+  });
+
+//@ts-ignore
+
+const Project = g
+  .model("Project", {
+    title: g.string().length({ min: 3 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteUrl: g.url(),
+    githubUrl: g.url(),
+    category: g.string().search(),
+    createdBy: g.relation(() => User),
+  })
+  .auth((rules) => {
+    rules.public().read(), rules.private().create().delete().update();
+  });
+
+const jwt = auth.JWT({
+  issuer: "grafbase",
+  secret: g.env("NEXTAUTH_SECRET"),
 });
 
-const Project = g.model('Project', {
-  title: g.string().length({min:3}),
-  description: g.string(),
-  image: g.url(),
-  liveSiteUrl: g.url(),
-  githubUrl: g.url(),
-  category: g.string().search(),
-  createdBy: g.relation(() => User)
-})
-
-
-
 export default config({
-  schema: g
-})
+  schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private(),
+  },
+});
